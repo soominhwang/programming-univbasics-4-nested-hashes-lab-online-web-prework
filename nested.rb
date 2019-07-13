b72fd56f9f74d068601d9bdef40555b9e93f24ca
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-13 03:42:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,11 @@ def adding_matz
         }
      }
 
-
+		 programmer_hash[:yukihiro_matsumoto] = {
+		 	:known_for=>"Ruby",
+		 	:languages=>["LISP", "C"]
+		 }
+		 return programmer_hash
 end
 
 def changing_alan
